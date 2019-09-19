cc52fa1e763afeaac76fb8abcf626a5fffdbdc83
Michael R. Essser <michaelresser@gmail.com>
2019-09-19 11:56:34 -0400

    index.html: added dialogue and image containers to Chapter 1 section; created and linked index.js file to index.html; index.css: began styling chapter 1 stuff. -MRE

---
+++ index.js
@@ -0,0 +1,12 @@
+//test script
+console.log('test');
+
+// Im laying out the named functions in order of their use across the games timeline, and trying to name them sematically. ie: theWoods1, theWoods2, ticTacToe, etc.
+
+const theWoods1 = () => { 
+    //1. Replace text in with the next scene's dialoge
+    document.getElementById('textBoxChapter1').innerHTML = 'You hear a bat futtering around chasing mosquitoes. Would you like to ask it for help?';
+    
+    //2. Replace the gameBox button with a button that says 'Ask the Bat for help!'
+
+}
