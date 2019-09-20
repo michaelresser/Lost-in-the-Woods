1ad3118a939a6a890f1ec6e12387af49e1eac9f6
Michael R. Essser <michaelresser@gmail.com>
2019-09-19 21:13:07 -0400

    index.html: refactored bootstrap classes to not suck; index.css: renamed classes and ids more semantically; index.js: began scripting ch1 story logic (see file for more details);

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
 //test script
 console.log('test');
 
-// Im laying out the named functions in order of their use across the games timeline, and trying to name them sematically. ie: theWoods1, theWoods2, ticTacToe, etc.
+/* 
+Im laying out the named functions in order of their use across the games timeline, and trying to name them sematically. ie: theWoods1, theWoods2, ticTacToe, etc.
+Below here, is my idea of mechanics more fleshed out. 
 
-const theWoods1 = () => { 
-    //1. Replace text in with the next scene's dialoge
-    document.getElementById('textBoxChapter1').innerHTML = 'You hear a bat futtering around chasing mosquitoes. Would you like to ask it for help?';
+The pattern is prety simple (fingers crossed!):  
+    1. Declare the functions list of actions.
+    2. Write the function that reacts to the target     button being pressed.
+    3. Drink a beer.
+
+-MRE
+*/
+
+// CHAPTER 1 - The Woods
+const theWoods0 = () => { 
+    //Resets the page.
+    //1. log a 'message when button is clicked'
+    console.log('theWoods1');
     
-    //2. Replace the gameBox button with a button that says 'Ask the Bat for help!'
+    //2. Text Reset
+    document.getElementById('ch1-text').innerHTML = 'You find yourself in a dark wood searching for the path home! You must find a way through this maze of trees, and make it to the river.';
 
-}
+    //3. Remove Back Button.
+    let parent = document.getElementById('ch1-controls');
+    let child = document.getElementById('back');
+    parent.removeChild(child);
+};
+document.getElementById('continue').on('click', theWoods0());
+
+const theWoods1 = () => {
+    //1. Log a console message when button is clicked.
+    console.log('theWoods1');
+
+    //2. Replace text in with the next scene's dialoge
+    document.getElementById('ch1-text').innerHTML = 'You hear a bat futtering around chasing mosquitoes. Would you like to ask it for help?';
+
+    //3. Add a Back button
+
+    document.getElementById('ch1-controls').append(`<button id='back' class=''
+    ></i>Back</button>`);
+};
+document.getElementById('continue').on('click', theWoods1());
+
