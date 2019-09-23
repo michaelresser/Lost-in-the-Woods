8df0647f885cc6d26af9e13aad92d2d1ac102db9
Michael R. Essser <michaelresser@gmail.com>
2019-09-23 15:03:17 -0400

    index.html: completed layout for lnding page, & 3 chapter story pages. -MRE

--- index.js
+++ index.js
@@ -14,13 +14,16 @@ The pattern is prety simple (fingers crossed!):
 */
 
 // CHAPTER 1 - The Woods
+// theWoods0 - 
 const theWoods0 = () => { 
     //Resets the page.
     //1. log a 'message when button is clicked'
     console.log('theWoods1');
     
     //2. Text Reset
-    document.getElementById('ch1-text').innerHTML = 'You find yourself in a dark wood searching for the path home! You must find a way through this maze of trees, and make it to the river.';
+    document.getElementById('ch1-text').innerHTML = "The woods are dark and a little scary. You hear all sorts of animal noises around
+    you. What could they be? As you enter a clearing, you see a small black bat hanging from a tree branch. He looks tired.</br>
+    Say hello to the bat?";
 
     //3. Remove Back Button.
     let parent = document.getElementById('ch1-controls');
@@ -29,15 +32,15 @@ const theWoods0 = () => {
 };
 document.getElementById('continue').on('click', theWoods0());
 
+// The Bat game
 const theWoods1 = () => {
     //1. Log a console message when button is clicked.
-    console.log('theWoods1');
+    console.log('ch1-01');
 
     //2. Replace text in with the next scene's dialoge
     document.getElementById('ch1-text').innerHTML = 'You hear a bat futtering around chasing mosquitoes. Would you like to ask it for help?';
 
     //3. Add a Back button
-
     document.getElementById('ch1-controls').append(`<button id='back' class=''
     ></i>Back</button>`);
 };
