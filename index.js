b98a361163a8a2c7af8d446718344a69c6ed8375
Michael R. Essser <michaelresser@gmail.com>
2019-10-04 11:10:16 -0400

    NAVBAR: im about to burn this thing down, and start all over. Still getting the error "cannot use classList on "null"  with "navbar-nav-items" dont know why. -MRE

--- index.js
+++ index.js
@@ -4,21 +4,21 @@ console.log('test');
 
 /* NAVBAR LOGIC */
 // Navbar Responsive menu
-let w = window.innerWidth;
-let navbarToggleButton = document.getElementById('navbar-toggle');
-// let navbarNavItems = document.getElementById('nav-items');
-// console.log(navbarNavItems.classList);
+    let w = window.innerWidth;
+    let navbarToggleButton = document.getElementById('navbar-toggle');
+    let navbarNavItems = document.getElementById('navbar-nav-items');
+    console.log(navbarNavItems.classList);
 
+    if (w <= 750) {
+        console.log('w: ' + w + ' < 750px');
+        navbarToggleButton.classList.remove('hidden');
+        navbarNavItems.classList.add('hidden')
+    } else {
+        console.log('w: ' + w + ' > 750px');
+        navbarToggleButton.classList.add('hidden');
+        navbarNavItems.classList.remove('hidden')
+    }
 
-if (w <= 750) {
-    console.log('w: ' + w + ' < 750px');
-    navbarToggleButton.classList.remove('hidden');
-    // navbarNavItems.classList.add('hidden')
-} else {
-    console.log('w: ' + w + ' > 750px');
-    navbarToggleButton.classList.add('hidden');
-    // navbarNavItems.classList.remove('hidden')
-}
 
 // Navbar Toggle script
 const navbarToggle = () => {
@@ -45,14 +45,14 @@ The pattern is prety simple (fingers crossed!):
 /* This is the code to change the text on the initial button push in the Bat Game
 */
 function batTextChange() {
-    
+
     var x = document.getElementById("ch1-text");
     if (x.innerHTML === "The woods are dark and a little scary. You hear all sorts of animal noises around you. What could they be? As you enter a clearing, you see a small black bat hanging from a tree branch. He looks tired.") {
         x.innerHTML = "You say hello to the bat.  He tries to look up, but can barely move his head. \“O-o-oh hello, small human,\” he says, \“can you help me?  I’ve been flying all night and all day and can’t find my cave!  I can’t see very well, but I can usually hear a waterfall near my home.  Can you help me listen for it?\” You nod, and begin listening.  You hear different things in each direction; which one is a waterfall?";
     } else {
         x.innerHTML = "The woods are dark and a little scary. You hear all sorts of animal noises around you. What could they be? As you enter a clearing, you see a small black bat hanging from a tree branch. He looks tired.";
     }
-    }
+}
 
 
 // CHAPTER 1 - The Bat Game - Matching sounds
