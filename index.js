e947ec29ed567f50b1d00c1c06584cc843e7d7a6
Michael R. Essser <michaelresser@gmail.com>
2019-10-30 16:38:18 -0400

    update ch1 design for better UX -MRE

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ console.log('test');
 
 
 /* NAVBAR LOGIC */
-
 // Navbar Responsive script    
 const responsiveNavbar = () => {
     if (window.innerWidth < 750) {
@@ -16,95 +15,86 @@ const responsiveNavbar = () => {
         document.getElementById('navbar-items').classList.remove('hidden');
     }
 };
-
 document.onload = responsiveNavbar();
 window.onresize = function () {
     responsiveNavbar();
 };
-
 //Navbar Toggle Script
 function navbarToggleHidden() {
     document.getElementById('navbar-items').classList.toggle('hidden');
 }
 
-
-
-
-
-/* GAME LOGIC SECTION
-Im laying out the named functions in order of their use across the games timeline, and trying to name them sematically. ie: theWoods1, theWoods2, ticTacToe, etc.
-Below here, is my idea of mechanics more fleshed out. 
-
-The pattern is prety simple (fingers crossed!):  
-    1. Declare the functions list of actions.
-    2. Write the function that reacts to the target button being pressed.
-    3. Repeat
-    4. Drink a beer.
-
--MRE
-*/
-
+/* GAME LOGIC SECTION*/
 // CHAPTER 1 - Bat Encounter
-/* This is the code to change the text on the initial button push in the Bat Game
-*/
-
 
 const bat00 = () => {
-    //CHANGE IMAGE
+    //Change Image
     document.getElementById('ch1-image').src = 'images/the-bat-02.png'
-
-    //CHANGE STORY TEXT
+    //Change Story Text
     let changedText = '<p class="text-center">You say hello to the bat.  He tries to look up, but can barely move his head. “O-o-oh hello, small human,” he says, “can you help me?  I’ve been flying all night and all day and can’t find my cave!  I can’t see very well, but I can usually hear a river near my home.  Can you help me listen for it?”';
     document.getElementById("ch1-text").innerHTML = changedText;
-
-    //CHANGE BUTTON TEXT
+    //Change Button Text
     let continueBtn = document.getElementById('ch1-btn-continue');
     continueBtn.innerHTML = 'Help the bat!';
     continueBtn.setAttribute('onclick', 'bat01()');
-
 }
 
-
 const bat01 = () => {
+    //Change Image
+    document.getElementById('ch1-image').src = 'images/the-bat-03.png'
     //Change Story Text
     let changedText = 'You nod, and begin listening.  You hear different things in each direction... which one is a river?';
     document.getElementById("ch1-text").innerHTML = changedText;
-
     //Change Image
     document.getElementById('ch1-image').src = 'images/the-bat-03.png'
-
-    //Remove Continue button
+    //Clear Buttons
     document.getElementById('ch1-btn-continue').remove();
     console.log('continue button removed');
-
     //Add Game Components
     document.getElementById('ch1-game').classList.remove('hidden');
     console.log('game component added');
-   
-
 }
 
+const playAudio = (event) => {
+    console.log(event.target);
+
+}
+const stopAudio = () => { };
 const playWest = () => {
     document.getElementById('sound01').play();
+}
+const stopWest = () => {
+    let audio = document.getElementById('sound01');
+    audio.pause();
+    audio.currentTime = 0;
 }
 const playNorth = () => {
     document.getElementById('sound02').play();
 }
+const stopNorth = () => {
+    let audio = document.getElementById('sound02');
+    audio.pause();
+    audio.currentTime = 0;
+}
 const playEast = () => {
     document.getElementById('sound03').play();
 }
-
-const batGameCheckWinner = () => {
-    let river = document.getElementById('ch1-game-river');    
-    if (river.checked === true) {
-        console.log('winner');
-        batGameFinale();
-        //     Including final Image, final text & continue button.     
-    } else {
-        console.log('loser');
-        // call function to tell player to try again.
-    }
-} 
+const stopEast = () => {
+    let audio = document.getElementById('sound03');
+    audio.pause();
+    audio.currentTime = 0;
+}
+// const batGameCheckWinner = () => {
+//     let river = document.getElementById('ch1-game-river');
+//     if (river.checked === true) {
+//         console.log('winner');
+//         batGameFinale();
+//         //     Including final Image, final text & continue button.     
+//     } else {
+//         console.log('loser');
+//         // call function to tell player to try again.
+//     }
+// }
 
 const batGameFinale = () => {
     //REMOVE GAEM ELEMENT
@@ -115,7 +105,7 @@ const batGameFinale = () => {
     // CHANGE TEXT
     document.getElementById('ch1-text').innerHTML = 'Well done!';
     // ADD CONTINUE BUTTON
-    document.getElementById('ch1-controls').insertAdjacentHTML('afterbegin',  "<button id='ch1-btn-continue' class=''><a href='#ch2'>Continue to Chapter 2</a></button>")
+    document.getElementById('ch1-controls').insertAdjacentHTML('afterbegin', "<button id='ch1-btn-continue' class=''><a href='#ch2'>Continue to Chapter 2</a></button>")
 }
 
 
